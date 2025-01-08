0a81615842123e9a48018ba0b594a15a21cf67b8
zandrij <ajosuesh@gmail.com>
2025-01-08 12:28:06 -0400

    asjute

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    
+
+    public function verify(Request $request)
+    {
+
+        return $request;
+        
+    }
+}
